588d126b15fd169c58899d6f7eb3ab974d50ff8c
kevinza <fkevinza@gmail.com>
2024-11-15 19:19:15 +0700

    final

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -51,12 +51,11 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+            Welcome to Virtual Lab ITB
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+            Web ini dibuat untuk memenuhi tugas mata kuliah "Pengembangan Aplikasi Web dan Mobile" dengan membuat sebuah media pembelajaran interaktif untuk mata kuliah TPB ITB
           </p>
         </div>
 
